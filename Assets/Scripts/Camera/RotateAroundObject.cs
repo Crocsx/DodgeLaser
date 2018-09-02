f8fe81a2171f632024bf87eb6ed3140780d08f8c
Crocsx <Crocsx.gaming@gmail.com>
2018-09-02 14:27:03 +0900

    v0.2 add menu prebeta

---
+++ Assets/Scripts/Camera/RotateAroundObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAroundObject : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.LookAt(target.position + offset);
+        transform.Translate(Vector3.right * Time.deltaTime);
+    }
+
+}
